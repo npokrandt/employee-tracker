54756bb32577ca48a5525018d930a85de8d4db4b
npokrandt <npokrandt919@gmail.com>
2023-09-07 17:29:59 -0500

    adds new is_manager column to employee table to be used for manager identification

--- db/schema.sql
+++ db/schema.sql
@@ -27,6 +27,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
+    is_manager BOOLEAN NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
--- helpers/add-employee.js
+++ helpers/add-employee.js
@@ -4,6 +4,13 @@ import inquirer from 'inquirer'
 import {init} from '../server.js'
 
 //employee has first and last name, role which is an id, and possibly a manager id
+
+//manager id is optional
+//could tag all possible managers (myself being the default)
+//modify employee table (add boolean for is_manager)
+//add similar thing here to get all manager names
+//make a list out of those, including N/A option
+//remember to include emp id to put in the proper field!
 export const addEmployee = () => {
 
     connection.connect(function(err) {
@@ -55,7 +62,13 @@ const inquirerAddEmployee = roleObjs => {
             type: 'number',
             name: 'employeeManager',
             message: 'Who is this employee\'s manager?',
-            default: 'NULL'
+            default: 0
+        },
+        {
+            type: 'confirm',
+            name: 'employeeIsManager',
+            message: 'Is this person a manager?',
+            default: false
         }
     ]).then((answers) => {
         let employeeRoleId;
@@ -65,17 +78,35 @@ const inquirerAddEmployee = roleObjs => {
             }
         }
 
+        let managerId = answers.employeeManager
+        
         connection.connect(function(err) {
-        if (err) throw err;
-        connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-                        VALUES 
-                        (?, ?, ?, ?)`, [answers.employeeFirstName, answers.employeeLastName, employeeRoleId, answers.employeeManager],
-            function (err, result, fields) {
-              if (err) throw err;
-              console.log('Employee Added!');
+            if (err) throw err;
+            
+            if (managerId === 0){
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id, is_manager) 
+                                VALUES 
+                                (?, ?, ?, ?)`, [answers.employeeFirstName, answers.employeeLastName, employeeRoleId,  answers.employeeIsManager],
+                    function (err, result, fields) {
+                      if (err) throw err;
+                      console.log('Employee Added!');
+        
+                      setTimeout(init, 2000)
+                });
 
-              setTimeout(init, 2000)
+            } else {
+
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id, is_manager) 
+                                VALUES 
+                                (?, ?, ?, ?, ?)`, [answers.employeeFirstName, answers.employeeLastName, employeeRoleId, answers.employeeManager, answers.employeeIsManager],
+                    function (err, result, fields) {
+                      if (err) throw err;
+                      console.log('Employee Added!');
+        
+                      setTimeout(init, 2000)
+                    });
+                }
             });
-        });
+            
     })
 }
